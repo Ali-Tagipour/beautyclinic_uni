129de329dc33b0ed43002f14d9e56938af353e21
ehsanghiyasi <ehsanghiyasi.clash@gmail.com>
2025-11-17 04:08:18 +0330

    Initial commit

---
+++ beautyclinic_uni/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace beautyclinic_uni.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
