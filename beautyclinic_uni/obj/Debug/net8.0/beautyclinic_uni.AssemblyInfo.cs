8e801995da05e9d8f6a3e3dcc9619e4d2313d0f2
ShadowStormLive <m.nooritab@gmail.com>
2025-11-29 19:13:47 +0300

    change formt of signup page

--- beautyclinic_uni/obj/Debug/net8.0/beautyclinic_uni.AssemblyInfo.cs
+++ beautyclinic_uni/obj/Debug/net8.0/beautyclinic_uni.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a4f6a6048f6c91f421f32c8a413f2ff74ae7e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b22aa7f6bf24b216d6810803c0cc6554ad697ffc")]
 [assembly: System.Reflection.AssemblyProductAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ beautyclinic_uni/wwwroot/js/js_signup.js
@@ -0,0 +1,11 @@
+﻿function togglePass() {
+    const p = document.getElementById("password");
+    p.type = p.type === "password" ? "text" : "password";
+}
+
+function submitReg(e) {
+    e.preventDefault();
+    const msg = document.getElementById("regMsg");
+    msg.textContent = "ثبت‌نام با موفقیت انجام شد (شبیه‌سازی).";
+    msg.style.color = "#065f46";
+}
