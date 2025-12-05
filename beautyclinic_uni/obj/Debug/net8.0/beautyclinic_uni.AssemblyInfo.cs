14568cbd2c834bd1f5c2dd511dda0ee327487b9b
ShadowStormLive <m.nooritab@gmail.com>
2025-12-06 01:31:08 +0300

    design ai chat bot ui in indexhtml

--- beautyclinic_uni/obj/Debug/net8.0/beautyclinic_uni.AssemblyInfo.cs
+++ beautyclinic_uni/obj/Debug/net8.0/beautyclinic_uni.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e801995da05e9d8f6a3e3dcc9619e4d2313d0f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+810356d565d92d78ca86138ec3204fe66a1abc51")]
 [assembly: System.Reflection.AssemblyProductAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
