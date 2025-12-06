7b79341e9d95a1149c4288ff1c4171920ae3a6f0
hamid reza karimian <hamidreza.karimian82@gmail.com>
2025-12-06 22:08:02 +0330

    write java of dasboard page

--- beautyclinic_uni/obj/Debug/net8.0/beautyclinic_uni.AssemblyInfo.cs
+++ beautyclinic_uni/obj/Debug/net8.0/beautyclinic_uni.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17b5a951f9d19ff07813626aa652fae080c6e5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43269de7f6638c37686d60a0cb63a32dd0662b47")]
 [assembly: System.Reflection.AssemblyProductAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
