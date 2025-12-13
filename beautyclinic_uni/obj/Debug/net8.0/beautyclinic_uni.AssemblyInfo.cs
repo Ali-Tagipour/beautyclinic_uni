4f02a47706dd3a3ed0b93743205196755c2a5939
hamid reza karimian <hamidreza.karimian82@gmail.com>
2025-12-13 17:10:35 +0330

    write html of clinic

--- beautyclinic_uni/obj/Debug/net8.0/beautyclinic_uni.AssemblyInfo.cs
+++ beautyclinic_uni/obj/Debug/net8.0/beautyclinic_uni.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92230babcdada0db35ac027669bef79775ebf83b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc3fd70c47245b89cc902fff4404de6c6cb5fcd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
