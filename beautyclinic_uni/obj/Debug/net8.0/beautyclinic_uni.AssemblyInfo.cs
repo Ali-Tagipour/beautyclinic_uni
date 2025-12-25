2fe8538030aa48e4286905de1dde217f28400f59
Ali-tagipour <97849366+Ali-Tagipour@users.noreply.github.com>
2025-12-25 12:32:25 +0330

    adding db to project

--- beautyclinic_uni/obj/Debug/net8.0/beautyclinic_uni.AssemblyInfo.cs
+++ beautyclinic_uni/obj/Debug/net8.0/beautyclinic_uni.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ece3108b7247e200cede4e828ead1831c884740")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3499d524f39ae4c185c8af953987b98655938000")]
 [assembly: System.Reflection.AssemblyProductAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beautyclinic_uni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
